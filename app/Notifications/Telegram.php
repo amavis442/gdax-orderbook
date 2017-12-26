47acd0482242f2d34ce78eec3b5312648cf52a8f
Patrick Teunissen <patrick.teunissen@crossmedia-club.nl>
2017-12-26 17:12:52 +0100

    Correction

--- app/Notifications/Telegram.php
+++ app/Notifications/Telegram.php
@@ -57,7 +57,7 @@ class Telegram extends Notification
                 '_Kosten: '. number_format($data['fee'],2) ."_\n";
 
         foreach ($this->balance as $name => $currency) {
-            $content .= '*Ballance '. $name. ' = ' .$currency.'*'."\n";
+            $content .= '*Balance '. $name. ' = ' .$currency.'*'."\n";
         }
 
         $content .= 'Aangemaakt op: '. $data['created_at'];
