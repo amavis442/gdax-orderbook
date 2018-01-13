c2735cda89d084a1a77ab085fd1f94b152199167
Patrick Teunissen <p.teunissen18@gmail.com>
2018-01-13 19:07:12 +0100

    Updated seeder

--- packages/amavis442/trading/src/database/seeds/OrderTableSeeder.php
+++ packages/amavis442/trading/src/database/seeds/OrderTableSeeder.php
@@ -22,7 +22,7 @@ class OrderTableSeeder extends Seeder
                                  'side'     => 'buy',
                                  'size'     => '0.001',
                                  'amount'   => $faker->numberBetween(12000, 12500),
-                                 'status'   => 'open',
+                                 'status'   => 'done',
                                  'order_id' => $faker->phoneNumber, // Order id format is like 191ca000-ac0a-427f-8c34-65d6e7c6b7be
                              ]);
         }
