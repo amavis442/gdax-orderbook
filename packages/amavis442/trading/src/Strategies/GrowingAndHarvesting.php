f00b6af1387f97049215787cf9eeeff33bb1faf7
Patrick Teunissen <p.teunissen18@gmail.com>
2018-01-23 21:58:33 +0100

    Control the bots operationmode

--- packages/amavis442/trading/src/Strategies/GrowingAndHarvesting.php
+++ packages/amavis442/trading/src/Strategies/GrowingAndHarvesting.php
@@ -32,63 +32,68 @@ class GrowingAndHarvesting implements Strategy
         $config = json_decode(Cache::get('bot::settings'));
         $stradle = Cache::get('bot::stradle', 0.03);
 
+        $lowerlimit = (float)$config->bottom;
+        $upperlimit = (float)$config->top;
+
+
         if (!is_null($currentprice)) {
-            $minimalSizeReached = false;
-            if ($fund > 0.01) { // Buy and use all of the fund
-                $price = $currentprice - (float)$stradle;
+
+            if ($currentprice > $lowerlimit &&
+                $currentprice < $upperlimit) {
 
 
+                $minimalSizeReached = false;
+                if ($fund > 0.01) { // Buy and use all of the fund
+                    $price = $currentprice - (float)$stradle;
 
-                $s = $fund / $price;
-                if ($s >= 0.001 && $pair == 'BTC-EUR') {
-                    $minimalSizeReached = true;
-                }
-
-                if ($s >= 0.01 && ($pair == 'ETH-EUR' || $pair == 'LTC-EUR')) {
-                    $minimalSizeReached = true;
-                }
-
-                $s = (string)$s;
-                $size = substr($s, 0,
-                    strpos($s, '.') + 9); // should be more then 0.0001 for BTC and 0.01 for ETH and LTC
-
-                if ($minimalSizeReached) {
-                    $result->put('side', 'buy');
-                    $result->put('size', $size);
-                    $result->put('price', number_format($price, 2, '.', ''));
-
-                    return $result->put('result', 'ok');
-                } else {
-                    $result->put('side', 'hold');
-                    $result->put('size', 0);
-                    $result->put('price', 0);
-                    $result->put('msg', 'Minimum size not reached '.$s);
-                    return $result->put('result', 'fail');
-                }
-            } else {
-                $lowerlimit = (float)$config->bottom;
-                $upperlimit = (float)$config->top;
-
-                // Needs an indicator to see if the trend goes up or not
-                if ($fund <= 0.01 &&
-                    $coin >= 0.0001 &&
-                    !is_null($position) &&
-                    $position->open < $currentprice &&
-                    $currentprice > $lowerlimit &&
-                    $currentprice < $upperlimit) {
-
-
-                    $price = $currentprice + (float)$stradle;
-                    if (!is_null($position) && !is_null($position->size)) {
-                        $size = $position->size;
-                    } else {
-                        $size = $config->size;
+                    $s = $fund / $price;
+                    if ($s >= 0.001 && $pair == 'BTC-EUR') {
+                        $minimalSizeReached = true;
                     }
-                    $result->put('side', 'sell');
-                    $result->put('size', $size);
-                    $result->put('price', number_format($price, 2, '.', ''));
 
-                    return $result->put('result', 'ok');
+                    if ($s >= 0.01 && ($pair == 'ETH-EUR' || $pair == 'LTC-EUR')) {
+                        $minimalSizeReached = true;
+                    }
+
+                    $s = (string)$s;
+                    $size = substr($s, 0,
+                        strpos($s, '.') + 9); // should be more then 0.0001 for BTC and 0.01 for ETH and LTC
+
+                    if ($minimalSizeReached) {
+                        $result->put('side', 'buy');
+                        $result->put('size', $size);
+                        $result->put('price', number_format($price, 2, '.', ''));
+
+                        return $result->put('result', 'ok');
+                    } else {
+                        $result->put('side', 'hold');
+                        $result->put('size', 0);
+                        $result->put('price', 0);
+                        $result->put('msg', 'Minimum size not reached ' . $s);
+
+                        return $result->put('result', 'fail');
+                    }
+                } else {
+                    // Needs an indicator to see if the trend goes up or not
+                    if ($fund <= 0.01 &&
+                        $coin >= 0.0001 &&
+                        !is_null($position) &&
+                        $position->open < $currentprice
+                    ) {
+
+
+                        $price = $currentprice + (float)$stradle;
+                        if (!is_null($position) && !is_null($position->size)) {
+                            $size = $position->size;
+                        } else {
+                            $size = $config->size;
+                        }
+                        $result->put('side', 'sell');
+                        $result->put('size', $size);
+                        $result->put('price', number_format($price, 2, '.', ''));
+
+                        return $result->put('result', 'ok');
+                    }
                 }
             }
         }
