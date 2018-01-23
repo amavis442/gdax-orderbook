efa9bf7071ab26d23770b6318bd4114deb813c40
Patrick Teunissen <p.teunissen18@gmail.com>
2018-01-23 23:53:09 +0100

    Buy price should be under last sell price of the last 5 minutes.

--- packages/amavis442/trading/src/Strategies/GrowingAndHarvesting.php
+++ packages/amavis442/trading/src/Strategies/GrowingAndHarvesting.php
@@ -43,6 +43,16 @@ class GrowingAndHarvesting implements Strategy
                 $currentprice > $lowerlimit &&
                 $currentprice < $upperlimit
             ) {
+                $timestamp = \Carbon\Carbon::now('Europe/Amsterdam')
+                                           ->subMinute(5)
+                                           ->format('Y-m-d H:i:s');
+
+                $order = \Amavis442\Trading\Models\Order::whereStatus('done')
+                                                        ->whereSide('sell')
+                                                        ->wherePair($pair)
+                                                        ->orderBy('id', 'desc')
+                                                        ->where('created_at', '>', $timestamp)
+                                                        ->first();
                 $minimalSizeReached = false;
                 if ($fund > 0.01) { // Buy and use all of the fund
                     $price = $currentprice - (float)$buystradle;
@@ -59,6 +69,12 @@ class GrowingAndHarvesting implements Strategy
                     $s = (string)$s;
                     $size = substr($s, 0,
                         strpos($s, '.') + 9); // should be more then 0.0001 for BTC and 0.01 for ETH and LTC
+
+                    if ($order) {
+                        if ($order->amount < $price) {
+                            $minimalSizeReached = false;
+                        }
+                    }
 
                     if ($minimalSizeReached) {
                         $result->put('side', 'buy');
@@ -119,14 +135,14 @@ class GrowingAndHarvesting implements Strategy
                 $order = \Amavis442\Trading\Models\Order::whereStatus('done')
                                                         ->whereSide('sell')
                                                         ->wherePair($pair)
-                                                        ->orderBy('id','desc')
+                                                        ->orderBy('id', 'desc')
                                                         ->first();
                 // When price goes down a lot and the last sell is way above currentprice and we have funds buy.
                 if ($fund >= 0.01 &&
                     ($order->amount - 80) > $currentprice
                 ) {
                     $minimalSizeReached = false;
-                    $price = $currentprice-0.01;
+                    $price = $currentprice - 0.01;
                     $s = $fund / $price;
 
                     if ($s >= 0.001 && $pair == 'BTC-EUR') {
