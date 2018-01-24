3d813b29629158b21ef3f5c274b4ecf567c1fa70
Patrick Teunissen <p.teunissen18@gmail.com>
2018-01-24 22:02:36 +0100

    Removed double call

--- resources/assets/js/components/Indicators.vue
+++ resources/assets/js/components/Indicators.vue
@@ -14,10 +14,18 @@
                 <td>{{ indicator.timestamp }}</td>
                 <td>
                     <span>
+                        <div v-if="indicator.indicatorsignal == 'sell'">
+                            <span class="alert alert-danger">{{ indicator.indicatorsignal }}</span>
+                        </div>
+
+                        <div v-if="indicator.indicatorsignal == 'buy'">
+                            <span class="alert alert-success">{{ indicator.indicatorsignal }}</span>
+                        </div>
+
                     {{ indicator.indicatorsignal }}
                     </span>
                 </td>
-                </tr>
+            </tr>
             </tbody>
         </table>
     </div>
@@ -31,16 +39,13 @@
         data() {
             return {
                 indicators: [
-                    {indicatorname: '' , indicatorsignal: 0, timestamp: ''}
+                    {indicatorname: '', indicatorsignal: 0, timestamp: ''}
                 ],
                 timer: ''
             }
         },
         created: function () {
             this.timer = setInterval(this.fetchIndicators(), 2000)
-        },
-        mounted() {
-            this.fetchIndicators();
         },
         methods: {
             fetchIndicators: function () {
