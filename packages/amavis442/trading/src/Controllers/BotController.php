f8a20b8a9ef8d9e06c5be1af774e0130570b701e
Patrick Teunissen <p.teunissen18@gmail.com>
2018-01-25 21:11:04 +0100

    Css for heartbeat

--- packages/amavis442/trading/src/Controllers/BotController.php
+++ packages/amavis442/trading/src/Controllers/BotController.php
@@ -24,7 +24,7 @@ class BotController extends Controller
     {
         $currentPrices = [];
         foreach (['BTC-EUR', 'ETH-EUR', 'LTC-EUR'] as $pair) {
-            $currentPrices[] = ['pair' => $pair, 'currentprice' => Cache::get('gdax::' . $pair . '::currentprice')];
+            $currentPrices[] = ['pair' => $pair, 'currentprice' => number_format(Cache::get('gdax::' . $pair . '::currentprice'),2,'.','')];
         }
 
         return ['currentprices' => $currentPrices];
